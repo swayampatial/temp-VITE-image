07004951bfeef944bb9819a7023d4ed18d696a5f
swayam patial <swayampatial1@gmail.com>
2024-11-07 21:33:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <Gallery />
+      <SearchForm />
+    </main>
+  );
+};
+export default App;
